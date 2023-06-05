2aae7cb27acc11e7c6121e94c52c8b923fadb6d2
card <cardenaso11@users.noreply.github.com>
2023-06-05 00:00:02 -0400

    fixes related to running hls on ghc 9.x (get hls from haskell.nix's flake input for it, instead of hackage like before, due to github.com/haskell.nix/issues/1830)

--- flake.nix
+++ flake.nix
@@ -12,6 +12,19 @@
 
     plutus-flake-utils = {
       url = "github:SundaeSwap-finance/plutus-flake-utils/rrruko/chap";
+      # this appears to be broken due to a bug in nix
+      # either that or i just do not understand
+      # in particular if you comment out the nixpkgs.follows line, nix flake lock becomes unable to generate a lockfile
+      # it works if you keep both lines but then the hls that gets produced doesnt work, and iirc it was equivalent to the hackage build one
+      # either way im not really sure what's going on
+      # there are multiple bugs open and closed on nix github wrt transitive dependencies so i feel like its likely it's just a bug
+      # alternatively, nixpkgs might be fine, and it might just be https://github.com/input-output-hk/haskell.nix/issues/1961 or https://github.com/input-output-hk/haskell.nix/issues/1830
+
+      #inputs = {
+      #  nixpkgs.follows = "nixpkgs";
+      #  haskell-nix.follows = "haskell-nix";
+      #};
+
     };
 
     flake-utils = {
@@ -52,10 +65,11 @@
         plutusProject = (plutus-flake-utils.plutusProject system (projectArgs false));
       in rec {
         pkgs = plutus-flake-utils.pkgs system;
-        inherit (plutusProject) flake project;
-        devShell = plutusProject.devShell.overrideAttrs (old: {
-          nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.static.haskellPackages.cabal-install pkgs.static.haskellPackages.haskell-language-server ];
-        });
+        inherit (plutusProject) flake project devShell;
+
+        #devShell = plutusProject.devShell.overrideAttrs (old: {
+        #  nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.static.haskellPackages.cabal-install pkgs.static.haskellPackages.haskell-language-server ];
+        #});
       }
     );
 }
