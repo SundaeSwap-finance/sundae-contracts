4cfc2730eaf10a6e8a06587de4d5d1080fcbf4e4
rrruko <rukokarasu@gmail.com>
2023-07-26 10:58:19 -0700

    implement settings boot mint policy

--- aiken/build.sh
+++ aiken/build.sh
@@ -6,6 +6,13 @@ set -e
 
 aiken build --keep-traces
 
+PROTOCOL_BOOT_UTXO="d8799fd8799f58200000000000000000000000000000000000000000000000000000000000000000ff00ff"
+aiken blueprint apply -v settings.spend $PROTOCOL_BOOT_UTXO > tmp
+mv tmp plutus.json
+
+aiken blueprint apply -v settings.mint $PROTOCOL_BOOT_UTXO > tmp
+mv tmp plutus.json
+
 SETTINGS_SCRIPT_HASH="581c$(aiken blueprint policy -v settings.mint)"
 aiken blueprint apply -v pool.spend $SETTINGS_SCRIPT_HASH > tmp
 mv tmp plutus.json
