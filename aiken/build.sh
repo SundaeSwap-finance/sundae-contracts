339220b546c194c0ceb4760b43032b557c231fad
rrruko <rukokarasu@gmail.com>
2023-11-27 08:33:25 -0800

    update boot utxo

--- aiken/build.sh
+++ aiken/build.sh
@@ -6,7 +6,7 @@ set -e
 
 aiken build
 
-PROTOCOL_BOOT_UTXO="d8799fd8799f5820f28f07c04cf5746eb24d3bf43c37a4e1f685728ec5695cbc6880c54a7589ac80ff04ff"
+PROTOCOL_BOOT_UTXO="d8799fd8799f5820ebcee8dcdbd7312f5e04a0033472465003617abe9935a6e56f007961897cfabbff01ff"
 aiken blueprint apply -v settings.spend $PROTOCOL_BOOT_UTXO > tmp
 mv tmp plutus.json
 
